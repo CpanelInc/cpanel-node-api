2db2c2141f79326a43517ecbc0b06304ca8d9405
Charan Angara <charan@cpanel.net>
2019-08-07 12:30:18 -0500

    replaced deep copy with reference logic

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,12 @@ const commonConfig = {
 };
 function buildConfig(env) {
     if (env === 'dev') {
-        // Object.assign doesn't do deep copy. Using JSON.parse as suggested in MDN.
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
-        let devConfigOptions = JSON.parse(JSON.stringify(commonConfig));
+        let devConfigOptions = commonConfig;
         devConfigOptions.mode = 'development';
         devConfigOptions.output.filename = 'cpanel-api.umd.js';
         return devConfigOptions;
     } else if (env === 'prod') {
-        let prodConfigOptions = JSON.parse(JSON.stringify(commonConfig));
+        let prodConfigOptions = commonConfig;
         prodConfigOptions.mode = 'production';
         prodConfigOptions.output.filename = 'cpanel-api.umd.min.js';
         return prodConfigOptions;

0feb6fadf8c966c190cf1458bfcbc42cb58dcee5
Charan Angara <charan@cpanel.net>
2019-08-07 12:06:21 -0500

    Build prod & dev artifacts when 'build' script is run.Fixed the deep copy issue with config object.

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,23 @@ const commonConfig = {
     entry: './dist/index.js',
     output: {
         path: path.resolve(__dirname, 'dist', 'bundles'),
-        filename: 'cpanel-api.umd.js',
         library: ['cp', 'api'],
         libraryTarget: 'umd'
     }
 };
 function buildConfig(env) {
     if (env === 'dev') {
-        return Object.assign({}, { 'mode': 'development' }, commonConfig);
+        // Object.assign doesn't do deep copy. Using JSON.parse as suggested in MDN.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
+        let devConfigOptions = JSON.parse(JSON.stringify(commonConfig));
+        devConfigOptions.mode = 'development';
+        devConfigOptions.output.filename = 'cpanel-api.umd.js';
+        return devConfigOptions;
     } else if (env === 'prod') {
-        let prodConfigOptions = {
-            'mode': 'production',
-            'output': {
-                filename: 'cpanel-locale.umd.min.js'
-            }
-        };
-        return Object.assign({}, commonConfig, prodConfigOptions);
+        let prodConfigOptions = JSON.parse(JSON.stringify(commonConfig));
+        prodConfigOptions.mode = 'production';
+        prodConfigOptions.output.filename = 'cpanel-api.umd.min.js';
+        return prodConfigOptions;
     } else {
         console.log("Wrong webpack build parameter. Possible choices: 'dev' or 'prod'.")
     }
